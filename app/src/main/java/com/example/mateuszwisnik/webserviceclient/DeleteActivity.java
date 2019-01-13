3497d13bee351b466260c1e7cea9fa06e9e952bd
mateuszwisnik <wisnikmateusz@gmail.com>
2019-01-13 14:46:42 +0100

    Add to repo

---
+++ app/src/main/java/com/example/mateuszwisnik/webserviceclient/DeleteActivity.java
@@ -0,0 +1,66 @@
+package com.example.mateuszwisnik.webserviceclient;
+
+import android.os.AsyncTask;
+import android.os.Looper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.InputStream;
+import java.net.URL;
+
+public class DeleteActivity extends AppCompatActivity {
+
+    private EditText editTextPostId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete);
+
+        editTextPostId = findViewById(R.id.editTextPostId);
+        Button buttonDelete = findViewById(R.id.buttonDelete);
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editTextPostId.getText().toString().equals("")) {
+                    Toast.makeText(DeleteActivity.this, "Enter post id", Toast.LENGTH_SHORT).show();
+                } else {
+                    makeRequest();
+                }
+            }
+        });
+    }
+
+    private void makeRequest() {
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(MainActivity.apiEndpoint + "/" + editTextPostId.getText().toString());
+                    HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                    httpsURLConnection.setRequestMethod("DELETE");
+                    Log.e("Response", httpsURLConnection.getResponseMessage());
+                    if(httpsURLConnection.getResponseCode() == 200) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(DeleteActivity.this, "Post successfully deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        httpsURLConnection.disconnect();
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mateuszwisnik/webserviceclient/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.mateuszwisnik.webserviceclient;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String apiEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonGetActivity = findViewById(R.id.buttonGetActivity);
+        Button buttonPostActivity = findViewById(R.id.buttonPostActivity);
+        Button buttonPutActivity = findViewById(R.id.buttonPutActivity);
+        Button buttonDeleteActivity = findViewById(R.id.buttonDeleteActivity);
+
+        buttonGetActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonPostActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PostActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonPutActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonDeleteActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DeleteActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
